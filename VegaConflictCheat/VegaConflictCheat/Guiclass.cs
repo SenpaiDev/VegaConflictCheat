6650407e90105a45904bee18ee1e68e0577ea966
SenpaiDev <frostbyte@ohyessenpaipleaseputit.in>
2016-03-20 23:14:08 +0100

    Initial Commit

---
+++ VegaConflictCheat/VegaConflictCheat/Guiclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace VegaConflictCheat {
+    class Guiclass : MonoBehaviour {
+        bool Menutog;
+        void OnGUI() {
+            if (Menutog) {
+                GUI.Box(new Rect(100, 100, 100, 100), "The injection Worked");
+            }
+        }
+        void Update() {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.J)) {
+                Menutog = !Menutog;
+            }
+        }
+    }
+}
+
---
+++ VegaConflictCheat/VegaConflictCheat/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace VegaConflictCheat
+{
+    public class Loader {
+        public static GameObject Loaderobj;
+        public static void Load() {
+            if (Loaderobj == null) {
+                Loaderobj = new GameObject();
+                Loaderobj.AddComponent<Guiclass>();
+                UnityEngine.Object.DontDestroyOnLoad(Loaderobj);
+            }
+        }
+    }
+}
